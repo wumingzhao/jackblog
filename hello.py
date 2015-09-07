8c4cc5a3953be84e664326bfb64ffb642aa8fb4c
wumingzhao <wumingzhao2031@hotmail.com>
2015-09-07 23:18:18 +0800

    use redirect

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,session,redirect,url_for
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
@@ -6,6 +6,7 @@ from wtforms import StringField,SubmitField
 from wtforms.validators import Required
 
 class NameForm(Form):
+   
     name=StringField('what is your name',validators=[Required()])
     submit=SubmitField('submit')
 
@@ -15,12 +16,11 @@ manage=Manager(app)
 bootstrap=Bootstrap(app)
 @app.route('/',methods=['GET','POST'])
 def index():
-    name=None
     form=NameForm()
     if form.validate_on_submit():
-        name=form.name.data
-        form.name.data=''
-    return render_template('index.html',form=form,name=name)
+        session['name']=form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form,name=session.get('name'))
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html',name=name)
