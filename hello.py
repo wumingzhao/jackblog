07cf29431a2c8f130257ff2bb638f1a49988fdec
wumingzhao <wumingzhao2031@hotmail.com>
2015-09-07 17:11:51 +0800

    add flask.script.Manager

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask.ext.script import Manager
 app=Flask(__name__)
+manage=Manager(app)
 @app.route('/')
 def index():
     return "<h1>hello world</h1>"
@@ -7,5 +9,5 @@ def index():
 def user(name):
     return "<h1>hello,%s</h1>"%name
 if __name__=='__main__':
-    app.run(debug=True)
+    manage.run()
 
