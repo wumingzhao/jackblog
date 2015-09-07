f9542e5f2ea78582cd93f02a6975523b8b731658
wumingzhao <wumingzhao2031@hotmail.com>
2015-09-07 17:36:13 +0800

    use Jinja2 model

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask.ext.script import Manager
 app=Flask(__name__)
 manage=Manager(app)
 @app.route('/')
 def index():
-    return "<h1>hello world</h1>"
+    return render_template('index.html')
 @app.route('/user/<name>')
 def user(name):
-    return "<h1>hello,%s</h1>"%name
+    return render_template('user.html',name=name)
 if __name__=='__main__':
     manage.run()
 
