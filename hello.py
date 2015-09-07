a77c7d168a54c103878cf06b227210a5cdc6b13d
wumingzhao <wumingzhao2031@hotmail.com>
2015-09-07 22:06:00 +0800

    add Class NameForm

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
 from flask import Flask,render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
+
+Class NameForm(Form):
+    name=StringField('what is your name',validators=[Required()])
+    submit=SubmitFiled('submit')
+
 app=Flask(__name__)
+app.config['SECRET_KEY']='you never guess'
 manage=Manager(app)
 bootstrap=Bootstrap(app)
 @app.route('/')
