95878df570f3ee332428cbfca88fb25c3ec121f9
wumingzhao <wumingzhao2031@hotmail.com>
2015-09-08 18:51:53 +0800

    create a datase

--- hello.py
+++ hello.py
@@ -1,23 +1,57 @@
-from flask import Flask,render_template,session,redirect,url_for
+import os
+from flask import Flask,render_template,session,redirect,url_for,flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
 from wtforms import StringField,SubmitField
 from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
 
+
+app=Flask(__name__)
+app.config['SECRET_KEY']='you never guess'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+#this is the config of database
+basedir=os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+db=SQLAlchemy(app)
+manage=Manager(app)
+bootstrap=Bootstrap(app)
+#this is a class to make a form
 class NameForm(Form):
    
     name=StringField('what is your name',validators=[Required()])
     submit=SubmitField('submit')
 
-app=Flask(__name__)
-app.config['SECRET_KEY']='you never guess'
-manage=Manager(app)
-bootstrap=Bootstrap(app)
+#define role model
+class Role(db.Model):
+    __tablename__='roles'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(64),unique=True)
+    users=db.relationship('User',backref='role')
+
+    def __repr__(self):
+        return '<Role %r>'%self.name
+
+#define user model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(64),unique=True,index=True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    
+    def __repr__(self):
+        return '<User %r>'%self.username
+
+
 @app.route('/',methods=['GET','POST'])
+
 def index():
     form=NameForm()
     if form.validate_on_submit():
+        old_name=session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('yo,you changed your name?') 
         session['name']=form.name.data
         return redirect(url_for('index'))
     return render_template('index.html',form=form,name=session.get('name'))
